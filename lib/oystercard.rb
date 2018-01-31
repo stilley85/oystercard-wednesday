f523e97c1a8de75b6ac8cc3e3556a96af912c50b
Stephen Tilley <stephentilley@gmail.com>
2018-01-31 13:16:08 +0000

    General refactoring and cleanup

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,27 +2,27 @@ class Oystercard
 
   attr_reader :balance, :in_use, :entry_station
 
-  MAXIMUM_BALANCE = 90
-  MINIMUM_FARE = 1
+  MAX_BALANCE = 90
+  MIN_FARE = 1
   def initialize
     @balance = 0
     @entry_station
   end
 
   def top_up(amount)
-    fail "Can not have more than £#{MAXIMUM_BALANCE}" if over_max?(amount)
+    fail "Can not have more than £#{MAX_BALANCE}" if over_max?(amount)
     @balance += amount
   end
 
   def touch_in(station)
     fail "Insufficient funds! Your balance is #{@balance},"\
-    " minimum fare is #{MINIMUM_FARE}" if minimum_fare?
+    " minimum fare is #{MIN_FARE}" if minimum_fare?
     # @in_use = true
     @entry_station = station
   end
 
   def touch_out
-    deduct(MINIMUM_FARE)
+    deduct(MIN_FARE)
     # @in_use = false
     @entry_station = nil
   end
@@ -34,11 +34,11 @@ class Oystercard
   private
 
   def over_max?(amount)
-    @balance + amount > MAXIMUM_BALANCE
+    @balance + amount > MAX_BALANCE
   end
 
   def minimum_fare?
-    @balance < MINIMUM_FARE
+    @balance < MIN_FARE
   end
 
   def deduct(amount)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,12 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
 
-  max_balance = Oystercard::MAXIMUM_BALANCE
-  minimum_fare = Oystercard::MINIMUM_FARE
+  max_balance = Oystercard::MAX_BALANCE
+  minimum_fare = Oystercard::MIN_FARE
 
   let(:station) { double("victoria") }
 
-  context 'initialize' do
+  context '#initialize' do
 
     it "initializes with a balance of 0" do
       expect(subject.balance).to eq(0)
@@ -23,14 +23,11 @@ describe Oystercard do
 
   end
 
-  context 'top_up card' do
-
-    it { is_expected.to respond_to(:top_up).with(1).argument }
+  context '#top_up' do
 
     it 'tops up card balance with given amount' do
       expect { subject.top_up(10) }.to change { subject.balance }.by(10)
-    end # this syntax works as well
-    # expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
 
     it 'prevents top up if that would exceed maximum_balance' do
       subject.top_up(max_balance)
@@ -39,20 +36,27 @@ describe Oystercard do
 
   end
 
-  context 'touching in and out' do
+  context 'touching in and out (money added to card)' do
 
     before(:each) do
       subject.top_up(max_balance)
       subject.touch_in(station)
     end
 
-    context "touch_in" do
+    context "#touch_in" do
+
       it "in_journey? changed to true when touch_in is called" do
         expect(subject.in_journey?).to eq(true)
       end
+
+      it 'touches in and remembers entry station' do
+        expect(subject.entry_station).to eq(station)
+      end
+
     end
 
-    context "touch_out" do
+    context "#touch_out" do
+
       it "in_journey? changed to false when touch_out is called" do
         subject.touch_out
         expect(subject.in_journey?).to eq(false)
@@ -62,23 +66,14 @@ describe Oystercard do
         subject.touch_out
         expect{ subject.touch_out }.to change{ subject.balance }.by(-minimum_fare)
       end
-    end
-  end
 
-  context 'testing touch_in(with_argument) and touch_out' do
-    before(:each) do
-      subject.top_up(max_balance)
-      subject.touch_in(station)
-    end
+      it 'touches out and sets entry_station to nil' do
+        subject.touch_out
+        expect(subject.entry_station).to eq(nil)
+      end
 
-    it 'touches in and remembers entry station' do
-      expect(subject.entry_station).to eq(station)
     end
 
-    it 'touches out and sets entry_station to nil' do
-      subject.touch_out
-      expect(subject.entry_station).to eq(nil)
-    end
   end
 
 end
